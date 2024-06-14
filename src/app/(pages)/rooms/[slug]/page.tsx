b05972e1912ba8e322571c56c6cec7abcd8abf6b
Esmin53 <esmin.tufekcic53@gmail.com>
2024-06-14 17:30:43 +0200

    Add sorting cards option

--- src/app/(pages)/rooms/[slug]/page.tsx
+++ src/app/(pages)/rooms/[slug]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import CardBack from "@/components/CardBack"
 import MyHand from "@/components/MyHand"
 import Table from "@/components/Table"
 import GameMenu from "@/components/gamemenu/GameMenu"
@@ -12,7 +13,7 @@ import { useEffect, useState } from "react"
 const page = () => {
 
     const [cardIds, setCardIds] = useState<number[] >([])
-    const [selectedCards, setSelectedCards] = useState<Card | null>(null)
+    const [selectedCards, setSelectedCards] = useState<Card[]>([])
     const [startingDeck, setStartingDeck] = useState<Card[]>([])
     const [lastDiscartedCard, setLastDiscartedCard] = useState<Card | null>(null)
     const [sortingMode, setSortingMode] = useState<boolean >(true)
@@ -32,17 +33,15 @@ const page = () => {
         }
     }
 
-    const selectCard = (cardId: Card) => {
-        let previousCard = selectedCards || null;
-        
-        if(cardId.id === selectedCards?.id) {
-            setSelectedCards(null)
-            return
+    const selectCard = (card: Card) => {
+        if (selectedCards.find(item => item.id === card.id)) {
+            // If the card is already selected, deselect it
+            setSelectedCards(prevSelected => prevSelected.filter(item => item.id !== card.id));
         } else {
-            setSelectedCards(cardId)
+            // Otherwise, select the card
+            setSelectedCards(prevSelected => [...prevSelected, card]);
         }
-
-    }
+    };
 
     const drawAcard = () => {
         if(cards.length >= 15) {
@@ -58,33 +57,59 @@ const page = () => {
     }
 
     const discardCard = () => {
-        if(selectedCards === null || cards.length === 14) {
+        if(selectedCards.length > 1) {
             return
         }
-        startingDeck.unshift(selectedCards)
-        setLastDiscartedCard(selectedCards)
-        setCards(cards.filter(item => item.id !== selectedCards.id))
-        setSelectedCards(null)  
+
+        if(!selectedCards.length || cards.length === 14) {
+            return
+        }
+        startingDeck.unshift(selectedCards[0])
+        setLastDiscartedCard(selectedCards[0])
+        setCards(cards.filter(item => item.id !== selectedCards[0].id))
+        setSelectedCards(selectedCards.filter((item => item.id !== selectedCards[0].id)))  
     }
 
     useEffect(() => {
         startGame()
     }, [])
 
+    const swapCards = () => {
+        if (selectedCards.length > 2 || selectedCards.length < 1) {
+             return
+        }
+
+        if(!cards[0]?.id || !cards[1]?.id) return
+        const index1 = cards.findIndex(card => card.id === selectedCards[0].id);
+        const index2 = cards.findIndex(card => card.id === selectedCards[1].id);
+
+        if (index1 === -1 || index2 === -1) {
+            console.error('One or both cards not found in the array');
+            return;
+        }
+
+        const newCards = [...cards];
+
+        [newCards[index1], newCards[index2]] = [newCards[index2], newCards[index1]];
+
+        setCards(newCards);
+        setSelectedCards(selectedCards.filter((item => item.id !== selectedCards[0].id)))
+
+    }
+
     return (
         <div className="flex-1 flex gap-2">
             <div className="flex-1 flex items-center flex-col pt-6 relative">
-                <div className="w-8 h-8 bg-peach rounded-full absolute top-2 right-2 cursor-pointer flex items-center justify-center"
-                onClick={() => setSortingMode(prev => !prev)}>
-                   <Recycle />
-                </div>
                 <Table drawCard={drawAcard} discardCard={discardCard}
                 lastDiscartedCard={lastDiscartedCard}
                 />
                 {cardIds?.length ? <MyHand 
                     selectedCards={selectedCards}
                     cards={cards} 
-                    selectCard={selectCard}/> : <p>Loading</p>}
+                    selectCard={selectCard}/> : <CardBack />}
+                <div className="w-full h-12 bg-[#486581] mt-auto flex items-center justify-center">
+                    <p className="text-paleblue font-medium text-lg cursor-pointer" onClick={() => swapCards()}>Swap</p>
+                </div>
             </div>
             <GameMenu />
         </div>
--- src/components/MyHand.tsx
+++ src/components/MyHand.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react"
 
 interface HandProps {
 
-    selectedCards: Card | null
+    selectedCards: Card[]
     selectCard: (card: Card) => void
     cards: Card[]
 }
@@ -14,12 +14,10 @@ const MyHand = ({selectedCards, selectCard, cards}: HandProps) => {
 
 
     return (
-        <div className="w-full h-fit  mt-auto flex -gap-20 justify-center my-6 absolute bottom-5">
-            {cards.map((item, index) => <div key={item.id} className={cn("hover:-my-4 duration-100", {
-                "-my-4": item.id === selectedCards?.id
-            })} onClick={() =>  selectCard(item)}>
+        <div className="w-full h-fit  mt-auto flex -gap-20 justify-center my-6 flex-shrink-0">
+            {cards.map((item, index) => <div key={item.id} className={cn("hover:-my-4 duration-100")} onClick={() =>  selectCard(item)}>
                 <CardFront card={item} 
-                className={item?.id === selectedCards?.id ? "border-red-500 -my-4 shadow-red-glow" : null}/>
+                className={selectedCards.find((sc) => item.id === sc.id ) ? "border-red-500 -my-4 shadow-red-glow" : null}/>
             </div>)}
         </div>
     )
